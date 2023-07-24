11e84eab0e069ab0d8e7216b50509cfd81029d66
Allyson Mitchem <allysonmitchem@gmail.com>
2023-07-24 09:36:28 -0600

    Function 1 - hello world

---
+++ 8kyu/function1-HelloWorld.js
@@ -0,0 +1,65 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// my solution
+
+function greet(){
+    const first = "hello"
+    const second = " world!"
+    let result = first + second
+    return result;
+  }
+// OR
+  const greet = () => "hello world!";
+
+//   other solutions 
+
+function greet() {
+
+    /*
+    quu..__
+     $$$b  `---.__
+      "$$b        `--.                          ___.---uuudP
+       `$$b           `.__.------.__     __.---'      $$$$"              .
+         "$b          -'            `-.-'            $$$"              .'|
+           ".                                       d$"             _.'  |
+             `.   /                              ..."             .'     |
+               `./                           ..::-'            _.'       |
+                /                         .:::-'            .-'         .'
+               :                          ::''\          _.'            |
+              .' .-.             .-.           `.      .'               |
+              : /'$$|           .@"$\           `.   .'              _.-'
+             .'|$u$$|          |$$,$$|           |  <            _.-'
+             | `:$$:'          :$$$$$:           `.  `.       .-'
+             :                  `"--'             |    `-.     \
+            :##.       ==             .###.       `.      `.    `\
+            |##:                      :###:        |        >     >
+       */return "hello world!";/*     |#'     `..'`..'          `###'        x:      /     /
+             \                                   xXX|     /    ./
+              \                                xXXX'|    /   ./
+              /`-.                                  `.  /   /
+             :    `-  ...........,                   | /  .'
+             |         ``:::::::'       .            |<    `.
+             |             ```          |           x| \ `.:``.
+             |                         .'    /'   xXX|  `:`M`M':.
+             |    |                    ;    /:' xXXX'|  -'MMMMM:'
+             `.  .'                   :    /:'       |-'MMMM.-'
+              |  |                   .'   /'        .'MMM.-'
+              `'`'                   :  ,'          |MMM<
+                |                     `'            |tbap\
+                 \                                  :MM.-'
+                  \                 |              .''
+                   \.               `.            /
+                    /     .:::::::.. :           /
+                   |     .:::::::::::`.         /
+                   |   .:::------------\       /
+                  /   .''               >::'  /
+                  `',:                 :    .'
+                                        
+    */
+      
+    
+    
+    }
